24481414b581dbfa3b11a8a0ca71605e6c28050a
Louis Magdaleno <buildthatapp@gmail.com>
2019-03-05 12:18:22 -0800

    Added PropTypes checking to Todo Component

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
+// TODO COMPONENT
+// * Receives props and stores them in state
+// * Props expected: 
+// *    todo (object)
+// *        contains:
+// *            task ( string )
+// *            id ( number )
+// *            completed ( boolean )
 class Todo extends Component {
     constructor(props) {
         super(props),
         this.state = { 
             task: props.todo.task,
             id: props.todo.id,
-            completed: props.todo.completed
+            completed: props.todo.completed,
+            key: props.todo.key
 
         };
     }
 
     render() {
         return (
-            <div className="todo">
+            <div className="todo" >
                 <h2>{this.state.task}</h2>
+                <p>{this.state.id}</p>
                 <p>{this.state.completed ? 'Done' : 'Not Done'}</p>
 
             </div>
         );
     }
 }
+
+// Typechecking props
+Todo.propTypes = {
+    todo: PropTypes.object,
+    task: PropTypes.string,
+    id: PropTypes.number,
+    completed: PropTypes.bool,
+    key: PropTypes.number
+};
 
 export default Todo;
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -24,7 +24,7 @@ class TodoList extends Component {
         return (
             <div className="todoList">
                 <h1>Todo List</h1>
-                {this.state.todos.map( todo => (<Todo todo={todo} />))}
+                {this.state.todos.map( todo => (<Todo todo={todo}  key={this.state.todos.id}/>))}
             </div>
         );
     }
